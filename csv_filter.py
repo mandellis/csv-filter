d240f8d9aaba0ac8ba22a64709fd33b0d7bb11c0
Simone Mandelli <dev@simonemandelli.eu>
2024-02-16 16:48:40 +0100

    Update path

--- csv_filter.py
+++ csv_filter.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-drs_01 = pd.read_csv("Lince/mid_clip.csv")
+drs_01 = pd.read_csv("D://VS_projects/Lince/mid_clip.csv")
 drs_01.rename(columns={'Points:0':'X','Points:1':'Y','Points:2':'Z','pressure-coefficient':'cp'},inplace=True)
 
-drs_02 = pd.read_csv("Lince/mid_clip.csv")
+drs_02 = pd.read_csv("D://VS_projects/Lince/mid_clip.csv")
 drs_02.rename(columns={'Points:0':'X','Points:1':'Y','Points:2':'Z','pressure-coefficient':'cp'},inplace=True)
 
-drs_03 = pd.read_csv("Lince/rear_clip.csv")
+drs_03 = pd.read_csv("D://VS_projects/Lince/rear_clip.csv")
 drs_03.rename(columns={'Points:0':'X','Points:1':'Y','Points:2':'Z','pressure-coefficient':'cp'},inplace=True)
 
-drs_04 = pd.read_csv("Lince/front_clip.csv")
+drs_04 = pd.read_csv("D://VS_projects/Lince/front_clip.csv")
 drs_04.rename(columns={'Points:0':'X','Points:1':'Y','Points:2':'Z','pressure-coefficient':'cp'},inplace=True)
 
 rho=1.225
@@ -19,15 +19,22 @@ V_id04 = 111.112
 V_id08 = 83.334
 
 def fx(x):
-    return x*0.5*rho*pow(V_id01,2)*1e-6
+    return x*0.5*rho*pow(V_id03,2)*1e-6
 
-#DRS 1 bounding box
-a=1518
-b=1835
+#DRS 1 bounding box close
+#a=1518
+#b=1835
+#c=-765
+#d=-445
+#e=30
+#f=125
+#DRS 1 bounding box open
+a=1516
+b=1836
 c=-765
 d=-445
-e=30
-f=125
+e=65
+f=85
 
 #filter data based on object bounding box
 inner01 = drs_01.query(f"{a} < X < {b} and {c} < Y < {d} and {e} < Z < {f}") 
@@ -39,13 +46,21 @@ print(inner01.head())
 
 inner01.to_csv('drs1.csv',index=False)
 
-#DRS 2 bounding box
+#DRS 2 bounding box close
+#a=2648
+#b=3235
+#c=-670
+#d=-340
+#e=320
+#f=485
+
+#DRS 2 bounding box open
 a=2648
 b=3235
 c=-670
 d=-340
-e=320
-f=485
+e=170
+f=360
 
 #filter data based on object bounding box
 inner02 = drs_02.query(f"{a} < X < {b} and {c} < Y < {d} and {e} < Z < {f}") 
@@ -57,13 +72,21 @@ print(inner02.head())
 
 inner02.to_csv('drs2.csv',index=False)
 
-#DRS 3 bounding box
+#DRS 3 bounding box close
+#a=4400
+#b=4650
+#c=-1085
+#d=0
+#e=820
+#f=925
+
+#DRS 3 bounding box open
 a=4400
 b=4650
 c=-1085
 d=0
 e=760
-f=900
+f=925
 
 #filter data based on object bounding box
 inner03 = drs_03.query(f"{a} < X < {b} and {c} < Y < {d} and {e} < Z < {f}") 
@@ -75,13 +98,21 @@ print(inner03.head())
 
 inner03.to_csv('drs3.csv',index=False)
 
-#DRS 4 bounding box
+#DRS 4 bounding box close
+#a=-875
+#b=-635
+#c=-702
+#d=0
+#e=75
+#f=140
+
+#DRS 4 bounding box open
 a=-875
 b=-635
 c=-702
 d=0
-e=75
-f=140
+e=35
+f=125
 
 #filter data based on object bounding box
 inner04 = drs_04.query(f"{a} < X < {b} and {c} < Y < {d} and {e} < Z < {f}") 
@@ -93,3 +124,6 @@ print(inner03.head())
 
 inner04.to_csv('drs4.csv',index=False)
 
+inner_finale = pd.concat([inner01,inner02,inner03,inner04])
+
+inner_finale.to_csv('drs.csv',index=False)
